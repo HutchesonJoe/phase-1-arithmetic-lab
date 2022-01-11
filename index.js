78385213f3c47fda1a4d9a9afad50499a766f429
Joe <hutcheson2001@yahoo.com>
2022-01-11 16:20:29 -0500

    commit tree 7830440a482a6bd2af6743c764a1479597533973

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+multiply;
+
+
+const firstNumber = Math.random() * 10;
+const random = (Math.ceil(firstNumber));
+random;
+
+const num3 = 19;
+const num4 = 5;
+const mod = 19 % 5;
+mod;
+
+const max = Math.max(1, 20)
+max; 
